fc817c1b1b55f2a6b627e8fcba5467a6dfd97e0a
Bérenger Thévenet <YiffyC@users.noreply.github.com>
2018-02-08 17:30:54 +0100

    blbl

---
+++ sous_Requetes.sql
@@ -0,0 +1,19 @@
+--SOUS REQUETES
+--4.1
+SELECT NOM
+FROM INGENIEUR I
+where CURRENT_DATE < ANY (select DATENT 
+			FROM EMPLOI E 
+                        WHERE I.MLE = E.MLE
+                        ORDER BY DATENT DESC)
+
+--4.2
+SELECT NOM
+FROM INGENIEUR I
+WHERE VILLE NOT IN
+	(SELECT VILLE 
+        FROM PROJET)
+AND SEXE = 'M';
+
+
+--4.3
