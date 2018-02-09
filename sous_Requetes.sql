1de711930be475ca10297eaff58ca56448d7207f
Bérenger Thévenet <YiffyC@users.noreply.github.com>
2018-02-09 10:26:21 +0100

    3 ok

--- sous_Requetes.sql
+++ sous_Requetes.sql
@@ -2,18 +2,29 @@
 --4.1
 SELECT NOM
 FROM INGENIEUR I
-where CURRENT_DATE < ANY (select DATENT 
-			FROM EMPLOI E 
+where CURRENT_DATE < ANY (select DATENT
+			FROM EMPLOI E
                         WHERE I.MLE = E.MLE
-                        ORDER BY DATENT DESC)
+                        ORDER BY DATENT DESC);
 
 --4.2
 SELECT NOM
 FROM INGENIEUR I
 WHERE VILLE NOT IN
-	(SELECT VILLE 
+	(SELECT VILLE
         FROM PROJET)
 AND SEXE = 'M';
 
 
 --4.3
+SELECT NOM
+FROM INGENIEUR I
+JOIN AFFECT A
+ON I.MLE = A.MLE
+WHERE VILLE =ALL(
+		SELECT VILLE
+                FROM PROJET P
+                JOIN AFFECT B
+                ON P.CODPRO = B.CODPRO
+                WHERE I.MLE= B.MLE
+                );
