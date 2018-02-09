0bc8bf0746abe3db93ab2366375010674d83fe6a
Bérenger Thévenet <berenger.thevenet@laposte.net>
2018-02-09 10:51:48 +0100

    Correction 1

--- sous_Requetes.sql
+++ sous_Requetes.sql
@@ -3,9 +3,19 @@
 SELECT NOM
 FROM INGENIEUR I
 where CURRENT_DATE < ANY (select DATENT
-			FROM EMPLOI E
+												FROM EMPLOI E
                         WHERE I.MLE = E.MLE
                         ORDER BY DATENT DESC);
+--Pas sur que ça soit la bonne
+
+--Correction pour marcier et gallet
+SELECT NOM
+FROM INGENIEUR I
+	INNER JOIN EMPLOI E
+	ON I.MLE = E.MLE
+	WHERE DATENT >= ALL
+		(SELECT DATENT FROM EMPLOI WHERE DATENT < '2018-01-01')
+AND DATENT < '2018-01-01'
 
 --4.2
 SELECT NOM
